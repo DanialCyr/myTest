6faead683a1a73198ce826229860f1c054669d60
DanialCyr <804029988@qq.com>
2020-08-15 16:44:28 +0800

    Password Incription

--- mainTest.py
+++ mainTest.py
@@ -1,10 +1,7 @@
-height=float(input("Please input your height (in meters):"))
-weight=float(input("Please input your weight (in kilograms)："))
-bmi=weight/(height*height)
-print("Your Body Mass Index is："+str(bmi))
-if bmi>18.5 and bmi<24.9:
-    print("You are underweight ~@_@~")
-if bmi>=24.9 and bmi<29.9:
-    print("Your weight is normal (-_-)")
-if bmi>=29.9:
-    print("You are overweight ^@_@^")
+password = 87654321
+key = 7
+print("\nOriginal Password：",password)
+password = password<<key
+print("\nAfter encryption:",password)
+password = password>>key
+print("\nAfter Decryption:",password)
