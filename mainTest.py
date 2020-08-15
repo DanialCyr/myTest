2981afb082cbbc27a20b30759c5a15bb07052783
DanialCyr <804029988@qq.com>
2020-08-15 15:09:07 +0800

    my first test code

---
+++ mainTest.py
@@ -0,0 +1,25 @@
+
+a = 'ligang'
+b = 'tiantian'
+
+print("ligang is tiantian's father")
+print(a)
+print(b)
+print(a+b)
+
+print(f"{a} is {b}'s father")
+
+c = 5
+d = c+5
+
+print(c)
+print(d)
+
+j = 'gigi'
+lichangjing = 'really cool'
+print(lichangjing)
+print("there's the sugar")
+print(f"{lichangjing}{j}")
+
+k = 7**10000
+print(k)
