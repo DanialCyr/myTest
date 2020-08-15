283dd3841708ad2c94082c0e8453f073cd3684da
DanialCyr <804029988@qq.com>
2020-08-15 16:00:44 +0800

    bmi test

--- mainTest.py
+++ mainTest.py
@@ -1,25 +1,10 @@
-
-a = 'ligang'
-b = 'tiantian'
-
-print("ligang is tiantian's father")
-print(a)
-print(b)
-print(a+b)
-
-print(f"{a} is {b}'s father")
-
-c = 5
-d = c+5
-
-print(c)
-print(d)
-
-j = 'gigi'
-lichangjing = 'really cool'
-print(lichangjing)
-print("there's the sugar")
-print(f"{lichangjing}{j}")
-
-k = 7**10000
-print(k)
+height=float(input("Please input your height (in meters):"))
+weight=float(input("Please input your weight (in kilograms)："))
+bmi=weight/(height*height)
+print("Your Body Mass Index is："+str(bmi))
+if bmi>18.5 and bmi<24.9:
+    print("You are underweight ~@_@~")
+if bmi>=24.9 and bmi<29.9:
+    print("Your weight is normal (-_-)")
+if bmi>=29.9:
+    print("You are overweight ^@_@^")
